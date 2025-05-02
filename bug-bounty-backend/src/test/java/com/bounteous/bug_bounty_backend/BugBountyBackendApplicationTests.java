150f4b4216fd12dfd6a9a8934742b35fea82ac69
Imad Issafras <imad.issafras@outlook.com>
2025-05-02 14:05:46 -0400

    setup frontend and backend folders

---
+++ bug-bounty-backend/src/test/java/com/bounteous/bug_bounty_backend/BugBountyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bounteous.bug_bounty_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBountyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
