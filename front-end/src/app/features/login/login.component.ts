b65ca279aedb897ccc1374afdf6f2655729e8603
Eric-CodeExplorer <eric.junlai@gmail.com>
2025-05-06 16:29:30 -0400

    implement login page

--- front-end/src/app/features/login/login.component.ts
+++ front-end/src/app/features/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../layout/nav-bar/nav-bar.component';
+import { CommonModule } from '@angular/common';
+import { InputBarComponent } from '../../shared/components/input-bar/input-bar.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NavBarComponent, CommonModule, InputBarComponent, ButtonComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  hasError: boolean = false;
+
+  OnUsernameChange(value: string) {
+    this.username = value;
+  }
+
+  OnPasswordChange(value: string) {
+    this.password = value;
+  }
 }
