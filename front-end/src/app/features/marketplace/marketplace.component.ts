4e602a6a96c04e70c493ca79fe2846d839265439
Eric-CodeExplorer <eric.junlai@gmail.com>
2025-05-07 10:59:24 -0400

    Create Feature Marketplace

---
+++ front-end/src/app/features/marketplace/marketplace.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SidebarLayoutComponent } from '../../layout/sidebar-layout/sidebar-layout.component';
+
+@Component({
+  selector: 'app-marketplace',
+  imports: [SidebarLayoutComponent],
+  templateUrl: './marketplace.component.html',
+  styleUrl: './marketplace.component.css',
+})
+export class MarketplaceComponent {}
