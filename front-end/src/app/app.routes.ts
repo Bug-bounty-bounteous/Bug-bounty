36b945238cfd46817461ddb242eff5c11ff8842c
Eric-CodeExplorer <eric.junlai@gmail.com>
2025-05-06 20:38:42 -0400

    Add route '/signup'

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { AboutComponent } from './features/about/about.component';
 import { LoginComponent } from './features/login/login.component';
+import { SignUpComponent } from './features/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', title: 'Home | Bug Bounty', component: HomeComponent },
   { path: 'about', title: 'About | Bug Bounty', component: AboutComponent },
   { path: 'login', title: 'Login | Bug Bounty', component: LoginComponent },
+  { path: 'signup', title: 'Sign Up | Bug Bounty', component: SignUpComponent },
 ];
