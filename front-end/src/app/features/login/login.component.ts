5ef12d587deb08efddecc1748220c6fdc45b7465
Eric-CodeExplorer <eric.junlai@gmail.com>
2025-05-06 20:39:12 -0400

    Bug fix

--- front-end/src/app/features/login/login.component.ts
+++ front-end/src/app/features/login/login.component.ts
@@ -3,10 +3,18 @@ import { NavBarComponent } from '../../layout/nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
 import { InputBarComponent } from '../../shared/components/input-bar/input-bar.component';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [NavBarComponent, CommonModule, InputBarComponent, ButtonComponent],
+  imports: [
+    NavBarComponent,
+    CommonModule,
+    InputBarComponent,
+    ButtonComponent,
+    FormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -16,11 +24,21 @@ export class LoginComponent {
 
   hasError: boolean = false;
 
+  constructor(private router: Router) {}
+
+  goToSignUp(event: MouseEvent) {
+    this.router.navigate(['/signup']);
+  }
+
   OnUsernameChange(value: string) {
     this.username = value;
   }
 
   OnPasswordChange(value: string) {
     this.password = value;
+  }
+
+  OnClickLogin(event: MouseEvent) {
+    console.log('Login logic here...');
   }
 }
