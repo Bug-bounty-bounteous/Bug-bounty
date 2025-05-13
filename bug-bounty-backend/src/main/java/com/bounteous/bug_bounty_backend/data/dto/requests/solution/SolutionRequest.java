7928757fd8c60a082b4dd6a1ef901b3354414f05
Priyanshi Singh <2000priyanshisingh@gmail.com>
2025-05-13 12:47:40 -0400

    upadtes

--- bug-bounty-backend/src/main/java/com/bounteous/bug_bounty_backend/data/dto/requests/solution/SolutionRequest.java
+++ bug-bounty-backend/src/main/java/com/bounteous/bug_bounty_backend/data/dto/requests/solution/SolutionRequest.java
@@ -2,11 +2,15 @@ package com.bounteous.bug_bounty_backend.data.dto.requests.solution;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.sql.Blob;
 
 // DTO for solution submission requests
 @Data
 public class SolutionRequest {
+
     @NotNull(message="BugId is required")
     private Long bugId;
 
