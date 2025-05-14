443a5dc8e2930ecf033545100370e410e2756ee6
Eric-CodeExplorer <eric.junlai@gmail.com>
2025-05-13 20:55:27 -0400

    add query to get developer's average rating

--- bug-bounty-backend/src/main/java/com/bounteous/bug_bounty_backend/data/repositories/bugs/FeedbackRepository.java
+++ bug-bounty-backend/src/main/java/com/bounteous/bug_bounty_backend/data/repositories/bugs/FeedbackRepository.java
@@ -2,10 +2,15 @@ package com.bounteous.bug_bounty_backend.data.repositories.bugs;
 
 import com.bounteous.bug_bounty_backend.data.entities.bugs.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 // Repository for Feedback entities
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
     // Repository methods will be added here
+    @Query("SELECT AVG(f.rating) FROM Feedback f WHERE f.solution.developer.id = :developerId")
+    Double findAverageRatingByDeveloperId(@Param("developerId") Long developerId);
 }
