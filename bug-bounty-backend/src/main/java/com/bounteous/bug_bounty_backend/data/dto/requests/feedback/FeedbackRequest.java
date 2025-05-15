c9017b525669563fb635d7f7343dd86f3f969df3
Priyanshi Singh <2000priyanshisingh@gmail.com>
2025-05-15 06:35:47 -0400

    Test

--- bug-bounty-backend/src/main/java/com/bounteous/bug_bounty_backend/data/dto/requests/feedback/FeedbackRequest.java
+++ bug-bounty-backend/src/main/java/com/bounteous/bug_bounty_backend/data/dto/requests/feedback/FeedbackRequest.java
@@ -1,9 +1,11 @@
 package com.bounteous.bug_bounty_backend.data.dto.requests.feedback;
 
 import lombok.Data;
+import lombok.Builder;
 
 // DTO for feedback submission requests
 @Data
+@Builder
 public class FeedbackRequest {
     private Long solutionId;
     private int rating;
